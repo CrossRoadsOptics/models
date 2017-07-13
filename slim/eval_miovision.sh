8703da0063a24e14e9f54f9b1560bbc078d03dcd
youralien <ryan.louie@students.olin.edu>
2017-07-12 22:12:42 -0400

    mobilenet trial 1 eval + momentum0.001 train

--- slim/eval_miovision.sh
+++ slim/eval_miovision.sh
@@ -1,4 +1,12 @@
-CHECKPOINT_FILE=/tmp/train_miovision_test/model.ckpt-17545
-DATASET_DIR=/data2/user_data/rlouie/miovision_cvpr
+CHECKPOINT_FILE=${HOME}/training/MIO-TCD-Classification/model.ckpt-61286
+# 2017-07-12 21:52:22.798495: I tensorflow/core/kernels/logging_ops.cc:79] eval/Accuracy[0.63713872]
+# 2017-07-12 21:52:22.798536: I tensorflow/core/kernels/logging_ops.cc:79] eval/Recall_5[0.95235068]
 
-python eval_image_classifier.py --alsologtostderr --checkpoint_path=${CHECKPOINT_FILE} --dataset_dir=${DATASET_DIR} --dataset_name=miovisioncvpr --dataset_split_name=validation --model_name=mobilenet_v1 
+# CHECKPOINT_FILE=${HOME}/training/MIO-TCD-Classification/model.ckpt-79632
+# 2017-07-12 22:10:10.217563: I tensorflow/core/kernels/logging_ops.cc:79] eval/Accuracy[0.49686897]
+# 2017-07-12 22:10:10.217754: I tensorflow/core/kernels/logging_ops.cc:79] eval/Recall_5[0.96533716]
+
+
+DATASET_DIR=${HOME}/datasets/MIO-TCD-Classification
+
+CUDA_VISIBLE_DEVICES=2 python eval_image_classifier.py --alsologtostderr --checkpoint_path=${CHECKPOINT_FILE} --dataset_dir=${DATASET_DIR} --dataset_name=miovisioncvpr --dataset_split_name=validation --model_name=mobilenet_v1 
--- slim/train_miovision.sh
+++ slim/train_miovision.sh
@@ -1,8 +1,8 @@
 DATASET_DIR=${HOME}/datasets/MIO-TCD-Classification
-TRAIN_DIR=${HOME}/training/MIO-TCD-Classification/
+TRAIN_DIR=${HOME}/training/MIO-momentum0.001
 CHECKPOINT_PATH=${HOME}/checkpoints/mobilenet_v1_1.0_128.ckpt
 
-python train_image_classifier.py \
+CUDA_VISIBLE_DEVICES=1 python train_image_classifier.py \
     --train_dir=${TRAIN_DIR} \
     --dataset_dir=${DATASET_DIR} \
     --dataset_name=miovisioncvpr \
@@ -11,4 +11,6 @@ python train_image_classifier.py \
     --train_image_size=128 \
     --checkpoint_path=${CHECKPOINT_PATH} \
     --checkpoint_exclude_scopes=MobilenetV1/Logits \
-    --trainable_scopes=MobilenetV1/Logits
+    --trainable_scopes=MobilenetV1/Logits \
+    --optimizer=momentum \
+    --learning_rate=0.001
