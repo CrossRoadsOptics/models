bd5a117317ea3949fd854fabf8c3c7ffcfb4d53a
youralien <ryan.louie@students.olin.edu>
2017-07-13 00:01:43 -0400

    Acc: 0.66, Recall: 0.97 mobilenet

---
+++ slim/train_miovision_mobilenet_best.sh
@@ -0,0 +1,16 @@
+DATASET_DIR=${HOME}/datasets/MIO-TCD-Classification
+TRAIN_DIR=${HOME}/training/MIO-momentum0.001
+CHECKPOINT_PATH=${HOME}/checkpoints/mobilenet_v1_1.0_128.ckpt
+
+CUDA_VISIBLE_DEVICES=0 python train_image_classifier.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=miovisioncvpr \
+    --dataset_split_name=train \
+    --model_name=mobilenet_v1 \
+    --train_image_size=128 \
+    --checkpoint_path=${CHECKPOINT_PATH} \
+    --checkpoint_exclude_scopes=MobilenetV1/Logits \
+    --trainable_scopes=MobilenetV1/Logits \
+    --optimizer=momentum \
+    --learning_rate=0.001
